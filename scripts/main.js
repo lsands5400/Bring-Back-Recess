20280fdc8fa7fb5265644764f849c20edf3eb513
lsands5400 <lsands@g.hmc.edu>
2025-10-18 21:24:16 -0700

    Added javascript main.js call in index.html.

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,6 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
+// Set default tab
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
