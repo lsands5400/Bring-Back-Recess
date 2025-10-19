fb61adb3aaac48128c098c8f52dcc2233f0b9936
lsands5400 <lsands@g.hmc.edu>
2025-10-18 21:20:23 -0700

    Adjusted some the tab characteristics, added default tab setting

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -16,6 +16,7 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
