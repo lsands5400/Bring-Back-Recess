04cc7228b584f46fe27580538def4584ea67f60a
lsands5400 <lsands@g.hmc.edu>
2025-10-28 14:23:32 -0700

    Added sign up button and modal to support it.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
+// Get the modal
+var modal = document.getElementById("signUpModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnsignup");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 function openTab(evt, tabName) {
   // Declare all variables
@@ -24,5 +50,4 @@ function openTab(evt, tabName) {
 window.onload = () => {
   document.getElementById("defaultOpen").click();
 };
-// Get the element with id="defaultOpen" and click on it
-// document.getElementById("defaultOpen").click();
+
